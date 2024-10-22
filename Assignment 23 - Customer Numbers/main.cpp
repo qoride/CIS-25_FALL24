822b95e7db302b80ef1b19500bc71df1f2d4f320
qoride <qoride@gmail.com>
2024-10-22 17:31:32 +0000

    first commit

--- Assignment 23 - Customer Numbers/main.cpp
+++ Assignment 23 - Customer Numbers/main.cpp
@@ -3,31 +3,24 @@
 
 using namespace std;
 
-int main(){
-    bool accepted;
-    int val;
-    string input;
-    Customer user;
-
-    cout << "Enter your ID: ";
-    user.ID = -1;
-
-    while(user.ID == -1){
-        while(cin >> val){
-            if(cin.good()){
-                user.ID = val;
-                break;
-            }
-        }
-        if(cin.fail()){
-            cin.clear();
-            cout << "\nERROR: ID was not accepted, try again: ";
-            cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            cin.clear();
-        }
+bool validateCustomer(string s){
+    if(s.size()!=6)return false;
+    for(int i = 0; i < s.size(); i++){
+        if(i<2&&isdigit(s[i]))return false;
+        if(i>1&&!isdigit(s[i]))return false;
     }
+    return true;
+}
 
-    cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-    cin.clear();
+int main(){
+    string input;
+
+    cout << "Enter your customer number: ";
+    getline(cin,input)
+
+    cout << "\nValidating..." << endl;
+
+    cout << "\nYour customer number is " << ((validateCustomer(input) == true)?"valid.":"not valid.") << endl;
+
     return 0;
 }
